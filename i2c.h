6e7849bba691c4b92fd8af1351db30b00b401986
Max <maksymilian.graff@gmail.com>
2023-09-13 21:26:45 +0200

    Update i2c.h

--- i2c.h
+++ i2c.h
@@ -15,15 +15,23 @@ void i2cMasterInit();
 
 void i2cSlaveInit(uint8_t slave_address);
 
+void i2cInit(uint8_t device_slave_address);
+
 void i2cERROR(uint8_t error_code );
 
 void i2cStart();
 
-void i2cWrite(uint8_t data);
+void i2cByteWrite(uint8_t data);
 
 void i2cStop();
 
-uint8_t data_recieved();
+uint8_t i2cSlaveDataReceived();
+
+uint8_t i2cRead(uint8_t addr, uint8_t register);
+
+void i2cWrite(uint8_t addr, uint8_t register, uint8_t data2write);
+
+uint8_t i2cReadAck();
 
 
 
